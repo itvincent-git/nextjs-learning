6d1870ec53c12263e83ef32eb1251573b2048b74
zhongyongsheng <itvincent@gmail.com>
2023-02-03 18:18:45 +0800

    chore(build): config export static html

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  //Error: Image Optimization using Next.js' default loader is not compatible with `next export`.
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
